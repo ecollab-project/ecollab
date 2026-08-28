5818332fd90ffc45b0760c74361ad3a80bdd37ef
ecollab-project <ecollab16@gmail.com>
2026-08-28 19:00:01 +0800

    test(M3): make error disclosure regression tests non-skippable

--- tests/Unit/ApiErrorResponderTest.php
+++ tests/Unit/ApiErrorResponderTest.php
@@ -12,16 +12,36 @@ final class ApiErrorResponderTest extends TestCase
 {
     public function testProductionMessageDoesNotExposeExceptionDetails(): void
     {
-        if (defined('APP_DEBUG')) {
-            $this->markTestSkipped('APP_DEBUG is already defined by the test bootstrap.');
+        if (!defined('APP_DEBUG')) {
+            define('APP_DEBUG', false);
         }
 
-        define('APP_DEBUG', false);
+        $this->assertFalse(
+            APP_DEBUG,
+            'ApiErrorResponder security tests must execute with APP_DEBUG=false; skipping would create false confidence.'
+        );
+
         $exception = new \RuntimeException('SQLSTATE[42S02]: secret_table does not exist');
 
         $this->assertSame(
             'Internal server error.',
             \ApiErrorResponder::publicMessage($exception, 'Internal server error.')
+        );
+    }
+
+    public function testGenericFallbackIsReturnedForArbitraryExceptionDetails(): void
+    {
+        if (!defined('APP_DEBUG')) {
+            define('APP_DEBUG', false);
+        }
+
+        $this->assertFalse(APP_DEBUG);
+
+        $exception = new \RuntimeException('PDO password=super-secret host=db.internal trace=private');
+
+        $this->assertSame(
+            'Request could not be completed.',
+            \ApiErrorResponder::publicMessage($exception, 'Request could not be completed.')
         );
     }
 }
