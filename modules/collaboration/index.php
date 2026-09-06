1fe8d116c9974b5c6254264c6a8492227da81a92
ecollab-project <ecollab16@gmail.com>
2026-09-06 19:09:45 +0800

    fix: route Collabs to server-wide workspace

--- modules/collaboration/index.php
+++ modules/collaboration/index.php
@@ -9,10 +9,12 @@ AuthMiddleware::startSession();
 AuthMiddleware::requireAuth();
 
 $channelId = (int)($_GET['channel_id'] ?? 0);
-$target = BASE_URL . '/modules/collaboration/coworkspaces.php';
-if ($channelId > 0) {
-    $target .= '?channel_id=' . rawurlencode((string)$channelId);
-}
+$serverId = (int)($_GET['server_id'] ?? 0);
+$target = BASE_URL . '/modules/collaboration/server-coworkspaces.php';
+$params = [];
+if ($channelId > 0) $params['channel_id'] = $channelId;
+if ($serverId > 0) $params['server_id'] = $serverId;
+if ($params) $target .= '?' . http_build_query($params);
 
 header('Cache-Control: no-store, private');
 header('Location: ' . $target, true, 302);
