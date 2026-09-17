13d7af092f33d27fe55f9b18b75b38be5447111b
Jerome Gabriel M Jumapao <ecollabadmin@srv1987643.hstgr.cloud>
2026-09-17 13:23:08 +0000

    fix: remove duplicate and backup migrations

--- database/migrations/042_resource_comment_permissions.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE collab_resource_permissions MODIFY COLUMN permission ENUM('view','comment','edit') NOT NULL DEFAULT 'view';
-ALTER TABLE collab_resource_invites MODIFY COLUMN permission ENUM('view','comment','edit') NOT NULL DEFAULT 'view';
