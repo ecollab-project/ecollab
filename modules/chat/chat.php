a6591046c16702241391c96052b1b84456a5035d
ecollab-project <ecollab16@gmail.com>
2026-09-23 21:10:34 +0800

    chore: bust thread enhancement cache

--- modules/chat/chat.php
+++ modules/chat/chat.php
@@ -3418,7 +3418,7 @@ $initials    = strtoupper(substr($user['full_name'] ?: $user['username'], 0, 1))
   <div id="pmCompatModal"></div>
 
   <script src="<?= BASE_URL ?>/assets/js/chat/functionality-overrides.js" defer></script>
-  <script src="<?= BASE_URL ?>/assets/js/chat/threads-enhancements.js?v=1" defer></script>
+  <script src="<?= BASE_URL ?>/assets/js/chat/threads-enhancements.js?v=imgfix2" defer></script>
   <script src="<?= BASE_URL ?>/assets/js/chat/collabs-ui.js" defer></script>
   <script src="<?= BASE_URL ?>/assets/js/chat/nav-persistence.js?v=1" defer></script>
 </body>
