696a44039037fae3433b3553e241bb4fb50ec94e
ecollab-project <ecollab16@gmail.com>
2026-09-24 22:00:06 +0800

    Use new eCollab wordmark on auth page

--- modules/auth/signup.php
+++ modules/auth/signup.php
@@ -45,10 +45,7 @@ if (!$isOAuthOnboarding) {
 
   <div class="page">
 
-    <a href="<?= BASE_URL ?>/index.php" class="nav-logo">
-      <div class="ico">🌿</div>
-      <?= APP_NAME ?>
-    </a>
+    <a href="<?= BASE_URL ?>/index.php" class="nav-logo ecollab-auth-brand"><img src="<?= BASE_URL ?>/assets/ecollab-wordmark.webp" alt="eCollab"></a>
 
     <div class="center">
       <div class="card signup-card">
